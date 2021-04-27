e74e030c313c157eb6236722439d07bd60f68d35
mdctenga <mdctenga@gmail.com>
2021-04-27 10:01:53 -1000

    firest commit

---
+++ cypress/integration/todomvc.spec.js
@@ -0,0 +1,14 @@
+/// <reference types = "cypress" />
+
+it('should be able to add a new todo to the list', () => {
+  cy.visit('http://todomvc-app-for-testing.surge.sh/');
+
+  cy.get('.new-todo',{timeout: 6000}).type("THIS IS WHAT POWER FEELS LIKE{enter}");
+
+  cy.get('label').should('have.text', 'THIS IS WHAT POWER FEELS LIKE');
+
+  cy.get('.toggle').should('not.be.checked');
+  // cy.get('.toggle').click();
+
+  // cy.contains('Clear completed').click();
+});
